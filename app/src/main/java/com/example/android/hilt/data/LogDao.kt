7352d61c3984dfa2767e551a680aab9f8383201b
markgray <mark.t.gray@gmail.com>
2022-03-28 07:37:31 -0400

    Added comments to several files.

--- app/src/main/java/com/example/android/hilt/data/LogDao.kt
+++ app/src/main/java/com/example/android/hilt/data/LogDao.kt
@@ -22,17 +22,44 @@ import androidx.room.Insert
 import androidx.room.Query
 
 /**
- * Data access object to query the database.
+ * Data access object to query the database. The `Dao` annotation marks this class as a Data Access
+ * Object. Data Access Objects are the main classes where you define your database interactions.
+ * They can include a variety of query methods. The class marked with `@Dao` should either be an
+ * interface or an abstract class. At compile time, Room will generate an implementation of this
+ * class when it is referenced by a Database (in our case this is [LogDao_Impl]).
  */
 @Dao
 interface LogDao {
 
+    /**
+     * The `Query` annotation marks a method in a Dao annotated class as a query method. The value
+     * of the annotation includes the query that will be run when this method is called. This query
+     * is verified at compile time by Room to ensure that it compiles fine against the database.
+     * The arguments of the method will be bound to the bind arguments in the SQL statement.
+     *
+     * This method returns all of the entries in the `logs` table of our database as a [List] of
+     * [Log] objects sorted in descending order by the [Log.id] field.
+     *
+     * @return a [List] of all of the [Log] objects in our database sorted in descending order by
+     * the [Log.id] field.
+     */
     @Query("SELECT * FROM logs ORDER BY id DESC")
     fun getAll(): List<Log>
 
+    /**
+     * The `Insert` annotation marks a method in a Dao annotated class as an insert method.
+     * The implementation of the method will insert its parameters into the database.
+     * All of the parameters of the Insert method must either be classes annotated with `Entity`
+     * or collections/array of it.
+     *
+     * @param logs the `vararg` or array of [Log] objects to insert in our database.
+     */
     @Insert
     fun insertAll(vararg logs: Log)
 
+    /**
+     * Deletes all of the rows in the `logs` table from the database.
+     */
     @Query("DELETE FROM logs")
     fun nukeTable()
 
