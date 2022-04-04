976ab82901c8b36bdea6e0b87d470691fc78b7a8
markgray <mark.t.gray@gmail.com>
2022-04-04 07:29:23 -0400

    Added comments to LogsFragment.kt

--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -67,8 +67,31 @@ class LogsFragment : Fragment() {
      */
     @Inject lateinit var dateFormatter: DateFormatter
 
+    /**
+     * This is the [RecyclerView] in our layout file with the ID [R.id.recycler_view] that we use to
+     * display the [Log] entries in our database.
+     */
     private lateinit var recyclerView: RecyclerView
 
+    /**
+     * Called to have the fragment instantiate its user interface view. This will be called between
+     * [onCreate] and [onViewCreated]. It is recommended to only inflate the layout in this method
+     * and move logic that operates on the returned [View] to [onViewCreated].
+     *
+     * We return the [View] that our [LayoutInflater] parameter [inflater] inflates from the layout
+     * file with ID [R.layout.fragment_logs] when it uses our [ViewGroup] parameter [container]
+     * for the `LayoutParams` without attaching to it.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-`null`, this is the parent [ViewGroup] that the fragment's
+     * UI will be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-`null`, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the [View] for the fragment's UI, or null.
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -77,16 +100,37 @@ class LogsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_logs, container, false)
     }
 
+    /**
+     * Called immediately after [onCreateView] has returned, but before any saved state has been
+     * restored in to the view. We initialize our [RecyclerView] field [recyclerView] by finding
+     * the [View] with ID [R.id.recycler_view] in our [View] parameter [view] and use the [apply]
+     * extension function to call its [RecyclerView.setHasFixedSize] method with the value `true`
+     * to inform it that adapter changes cannot affect the size of the [RecyclerView] (which allows
+     * it to perform several optimizations).
+     *
+     * @param view The [View] returned by [onCreateView].
+     * @param savedInstanceState If non-`null`, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view).apply {
             setHasFixedSize(true)
         }
     }
 
+    /**
+     * Called when the fragment is visible to the user and actively running. First we call our
+     * super's implementation of `onResume`, then we call the [LoggerDataSource.getAllLogs] method
+     * of our field [logger] with a lambda that sets the adapter of our [RecyclerView] field
+     * [recyclerView] to an instance of [LogsViewAdapter] constructed to use the [List] of [Log]
+     * objects retrieved from [logger] and to format the [Log.timestamp] of each [Log] using our
+     * [DateFormatter] field [dateFormatter] when it includes the timestamp in the text that is
+     * displayed in the item view used to display the [Log].
+     */
     override fun onResume() {
         super.onResume()
 
-        logger.getAllLogs { logs ->
+        logger.getAllLogs { logs: List<Log> ->
             recyclerView.adapter =
                 LogsViewAdapter(
                     logs,
@@ -98,14 +142,35 @@ class LogsFragment : Fragment() {
 
 /**
  * RecyclerView adapter for the logs list.
+ *
+ * @param logsDataSet the [List] of [Log] objects that will serve as our dataset.
+ * @param daterFormatter the [DateFormatter] to use to format the [Log.timestamp] field when
+ * we display each [Log] object in a [TextView].
  */
 private class LogsViewAdapter(
     private val logsDataSet: List<Log>,
     private val daterFormatter: DateFormatter
 ) : RecyclerView.Adapter<LogsViewAdapter.LogsViewHolder>() {
 
+    /**
+     * This is the custom [RecyclerView.ViewHolder] we use to hold each [TextView] that we use as
+     * the [itemView] of the [Log] objects in our dataset.
+     *
+     * @param textView the [TextView] that displays the info contained in our [Log] entry.
+     */
     class LogsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
+    /**
+     * Called when [RecyclerView] needs a new [RecyclerView.ViewHolder] of the given type to represent
+     * an item. We return the [View] that the [LayoutInflater] from the context of our [ViewGroup]
+     * parameter [parent] inflates from the layout file with ID [R.layout.text_row_item] when it uses
+     * our [ViewGroup] parameter [parent] for the `LayoutParams` without attaching to it.
+     *
+     * @param parent The [ViewGroup] into which the new [View] will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new [View].
+     * @return A new [LogsViewHolder] that holds a [View] of the given view type.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsViewHolder {
         return LogsViewHolder(
             LayoutInflater.from(parent.context)
@@ -113,13 +178,31 @@ private class LogsViewAdapter(
         )
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter. We just return the
+     * size of our [List] of [Log] field [logsDataSet].
+     *
+     * @return The total number of items in this adapter.
+     */
     override fun getItemCount(): Int {
         return logsDataSet.size
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [LogsViewHolder.itemView] to reflect the item at the given
+     * position. We initialize our [Log] variable `val log` to the entry at position [position]
+     * in our [List] of [Log] field [logsDataSet], then set the text of the [TextView] in our
+     * [LogsViewHolder] parameter [holder] to a string formed from the information contained in the
+     * fields of `log`.
+     *
+     * @param holder The [LogsViewHolder] which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: LogsViewHolder, position: Int) {
-        val log = logsDataSet[position]
+        val log: Log = logsDataSet[position]
         holder.textView.text = "${log.msg}\n\t${daterFormatter.formatDate(log.timestamp)}"
     }
 }
