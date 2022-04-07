158130df2c20d03b10c57c66bf8a848a729ba9e2
markgray <mark.t.gray@gmail.com>
2022-04-07 06:36:31 -0400

    Added comments to CustomTestRunner.kt

--- app/src/androidTest/java/com/example/android/hilt/CustomTestRunner.kt
+++ app/src/androidTest/java/com/example/android/hilt/CustomTestRunner.kt
@@ -20,8 +20,26 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
+import kotlin.reflect.KClass
 
+/**
+ * This is the custom `testInstrumentationRunner` that needs to be used for running instrumented
+ * tests on a Hilt injected application and the app/build.gradle specifies it as such in the
+ * `defaultConfig` block of its `android` block.
+ */
+@Suppress("unused") // Used in app/build.gradle
 class CustomTestRunner : AndroidJUnitRunner() {
+    /**
+     * Perform instantiation of the process's [Application] object. The default implementation
+     * provides the normal system behavior, and the only change necessary is passing the name
+     * of the java class instance corresponding to the [KClass] instance [HiltTestApplication].
+     *
+     * @param cl The ClassLoader with which to instantiate the object.
+     * @param name The name of the class implementing the Application object.
+     * @param context The context to initialize the application with
+     *
+     * @return The newly instantiated Application object.
+     */
     override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
         return super.newApplication(cl, HiltTestApplication::class.java.name, context)
     }
