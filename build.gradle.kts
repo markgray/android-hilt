710f533513c347c6b122fad25a1084281f1e94c1
markgray <mark.t.gray@gmail.com>
2024-07-05 06:51:06 -0400

    Converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in main branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,28 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    ext.kotlin_version = "1.9.24"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-        classpath ("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.24-1.0.20")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete) {
-    delete rootProject.layout.buildDir
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 }
