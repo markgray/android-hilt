b705d2db90f39f8c01957acd4c3406b96c3c49fc
markgray <mark.t.gray@gmail.com>
2025-03-06 07:57:55 -0500

    Updated AGP to 8.9.0 and updated gradle dependencies in solution branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     // Room
@@ -65,6 +65,6 @@ dependencies {
     ksp("com.google.dagger:hilt-android-compiler:2.55")
 
     // Hilt testing dependencies
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.53.1")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.55")
     kspAndroidTest("com.google.dagger:hilt-android-compiler:2.55")
 }
