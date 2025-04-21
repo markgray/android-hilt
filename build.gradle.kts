62662084cf82e475e27ca6ecdfb64375073630f6
Mark Gray <mark.t.gray@gmail.com>
2025-04-21 03:31:36 -0400

    Updated gradle dependencies in myMain branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,6 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.google.dagger.hilt.android") version "2.56" apply false
+    id("com.google.dagger.hilt.android") version "2.56.1" apply false
     id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
 }
